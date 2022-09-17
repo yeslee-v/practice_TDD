ade61570f3d054de2ac9dc20e0f2579cc75837df
Yeshin Lee <yeslee5004@gmail.com>
2022-09-17 20:58:35 +0900

    Get user id

--- index.js
+++ index.js
@@ -13,12 +13,23 @@ app.use(morgan("dev"));
 
 app.get("/users", (req, res) => {
   req.query.limit = req.query.limit || 10;
-  console.log("req: ", req.query.limit);
   const limit = parseInt(req.query.limit, 10);
   if (Number.isNaN(limit)) {
     return res.status(400).end();
   }
   res.json(users.slice(0, limit));
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id)) {
+    return res.status(400).end();
+  }
+  const user = users.filter((user) => user.id === id)[0];
+  if (!user) {
+    return res.status(404).end();
+  }
+  res.json(user);
 });
 
 app.listen(port, () => {
--- index.spec.js
+++ index.spec.js
@@ -2,29 +2,50 @@ const request = require("supertest");
 const should = require("should");
 const app = require("./index");
 
-describe("GET /users는", () => {
+// describe("GET /users는", () => {
+//   describe("Success", () => {
+//     it("Response user object array", (done) => {
+//       request(app)
+//         .get("/users")
+//         // .expect(400)
+//         .end((err, res) => {
+//           res.body.should.be.instanceOf(Array);
+//           done(); // async
+//         });
+//     });
+//     it("Response max limited", (done) => {
+//       request(app)
+//         .get("/users?limit=2")
+//         .end((err, res) => {
+//           res.body.should.have.lengthOf(2);
+//           done(); // async
+//         });
+//     });
+//   });
+//   describe("Fail", () => {
+//     it("Response 400 if limit is not integer", (done) => {
+//       request(app).get("/users?limit=two").expect(400).end(done);
+//     });
+//   });
+// });
+
+describe("GET /users/1은", () => {
   describe("Success", () => {
-    it("Response user object array", (done) => {
+    it("Response object that id is 1", (done) => {
       request(app)
-        .get("/users")
-        // .expect(400)
+        .get("/users/1")
         .end((err, res) => {
-          res.body.should.be.instanceOf(Array);
-          done(); // async
-        });
-    });
-    it("Response max limited", (done) => {
-      request(app)
-        .get("/users?limit=2")
-        .end((err, res) => {
-          res.body.should.have.lengthOf(2);
-          done(); // async
+          res.body.should.have.property("id", 1);
+          done();
         });
     });
   });
   describe("Fail", () => {
-    it("Response 400 if limit is not integer", (done) => {
-      request(app).get("/users?limit=two").expect(400).end(done);
+    it("Response if id is not integer", (done) => {
+      request(app).get("/users/one").expect(400).end(done);
+    });
+    it("Response if user does not exist", (done) => {
+      request(app).get("/users/4").expect(404).end(done);
     });
   });
 });
